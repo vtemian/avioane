8a546eec41198f760028949edb025d021f8b5ca5
Vlad Temian <wok@wok-Satellite-L500.(none)>
2012-05-25 01:02:06 +0300

    lobby page ready

--- game/views.py
+++ game/views.py
@@ -7,20 +7,20 @@ from division.views import Divisions
 def start(request):
     context = user_menu(request)
 
-    user_division = UserDivision.objects.get(user=context['userprofile'])
 
     divisions = Divisions()
-    prev_division, next_division = divisions.next_division(user_division.name)
+    prev_division, next_division = divisions.next_division(context['division'].name)
 
     context['next_division'] = next_division
     context['prev_division'] = prev_division
 
-    context['avion'] = divisions.get_division_by_name(user_division.name)['plane_type']
+    context['avion'] = divisions.get_division_by_name(context['division'].name)['plane_type']
 
     division_users = UserDivision.objects.filter(name=context['division'].name).order_by('points', '-matches_played')
 
     context['division_users'] = division_users
-
+    remaining_games = divisions.get_division_by_name(context['division'].name)['matches'] - context['division'].matches_played
+    context['remaining_games'] = remaining_games
     return render_to_response('game.html',
                               context,
                               context_instance=RequestContext(request))
